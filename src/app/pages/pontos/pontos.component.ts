a56d0e23f4e995f89c102814b9efb8b173ccfb11
TheoSilvaSa <03silva.theo@gmail.com>
2025-12-03 20:59:03 -0300

    fix: CRUD Pontos

--- src/app/pages/pontos/pontos.component.ts
+++ src/app/pages/pontos/pontos.component.ts
@@ -8,20 +8,48 @@ import { MessageService } from 'primeng/api';
 })
 export class PontosComponent implements OnInit {
   lista: any[] = [];
+  bairros: any[] = [];
+  
   ponto: any = {};
+  bairroSelecionadoId: number | null = null;
+  
   modalAberto = false;
 
   constructor(private api: ApiService, private msg: MessageService) {}
 
-  ngOnInit() { this.carregar(); }
-  carregar() { this.api.listarPontos().subscribe(d => this.lista = d); }
+  ngOnInit() { 
+    this.carregar(); 
+    this.carregarBairros();
+  }
+  
+  carregar() { 
+    this.api.listarPontos().subscribe(d => this.lista = d); 
+  }
+
+  carregarBairros() {
+    this.api.listarBairros().subscribe(b => this.bairros = b);
+  }
   
   abrirModal(item?: any) {
     this.ponto = item ? { ...item } : {};
+    
+    if (this.ponto.bairro && this.ponto.bairro.id) {
+        this.bairroSelecionadoId = this.ponto.bairro.id;
+    } else {
+        this.bairroSelecionadoId = null;
+    }
+    
     this.modalAberto = true;
   }
 
   salvar() {
+    if (!this.bairroSelecionadoId) {
+        this.msg.add({severity:'warn', summary:'Atenção', detail: 'Selecione um bairro.'});
+        return;
+    }
+
+    this.ponto.bairro = { id: this.bairroSelecionadoId };
+
     this.api.salvarPonto(this.ponto).subscribe({
       next: () => {
         this.modalAberto = false;
